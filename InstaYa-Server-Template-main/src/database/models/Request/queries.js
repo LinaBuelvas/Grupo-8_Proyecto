05065593fb5ddf39bea81a2e83a22a100a3a290d
LinaBuelvas <linabuelvas09@gmail.com>
2022-12-08 23:16:49 -0500

    version1

---
+++ InstaYa-Server-Template-main/src/database/models/Request/queries.js
@@ -0,0 +1,82 @@
+import { Request } from '@models/Request';
+
+export async function findRequestsByUser(userId) {
+	return await Request.find({ fromOwner: userId });
+}
+
+export async function saveRequest(userId, body) {
+	const {
+		isFragile,
+		width,
+		height,
+		depth,
+		weight,
+		due,
+		fromCity,
+		fromAddress,
+		toCity,
+		toAddress,
+		toOwner,
+		toOwnerId,
+	} = body;
+
+	const payload = {
+		fromOwner: userId,
+		isFragile,
+		width,
+		height,
+		depth,
+		weight,
+		due,
+		fromCity,
+		fromAddress,
+		toCity,
+		toAddress,
+		toOwner,
+		toOwnerId,
+	};
+
+	const request = new Request(payload);
+	await request.save();
+	return request;
+}
+
+
+// export async function updateRequest(userId, body) {
+// 	const {
+// 		id,
+// 		isFragile,
+// 		width,
+// 		height,
+// 		depth,
+// 		weight,
+// 		due,
+// 		fromCity,
+// 		fromAddress,
+// 		toCity,
+// 		toAddress,
+// 		toOwner,
+// 		toOwnerId,
+// 	} = body;
+
+// 	const payload = {
+// 		id,
+// 		fromOwner: userId,
+// 		isFragile,
+// 		width,
+// 		height,
+// 		depth,
+// 		weight,
+// 		due,
+// 		fromCity,
+// 		fromAddress,
+// 		toCity,
+// 		toAddress,
+// 		toOwner,
+// 		toOwnerId,
+// 	};
+
+// 	const request = new Request(payload);
+// 	await request.update();
+// 	return request;
+// }
---
+++ InstaYa-Server-Template-main/src/server/api/controllers/request/router.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import { findRequestsByUser, saveRequest, updateRequest } from '@models/Request/queries';
+import { logger, validate } from '../../middlewares';
+import { hasSession } from '../../middlewares/authorization';
+import { newRequestSchema } from './schemas';
+
+const router = express.Router();
+
+router.use(logger);
+
+router.get('/find-all', hasSession, async (req, res) => {
+	try {
+		const query = await findRequestsByUser(req.session_payload.id);
+
+		res.status(200).json({ payload: query, message: 'Requests fetched' });
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.error(error);
+		res.status(500).json(error.message);
+	}
+});
+
+router.post(
+	'/',
+	hasSession,
+	validate(newRequestSchema),
+	async ({ body, session_payload }, res) => {
+		try {
+			const payload = await saveRequest(session_payload.id, body);
+
+			res.status(200).json({ payload, message: 'Request saved!' });
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
+			res.status(500).json(error.message);
+		}
+	},
+);
+
+
+
+
+router.put(
+	'/edit',
+	hasSession,
+	validate(newRequestSchema),
+	async ({ body, session_payload }, res) => {
+		try {
+			const payload = await updateRequest(session_payload.id, body);
+
+			res.status(200).json({ payload, message: 'Request Update!' });
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
+			res.status(500).json(error.message);
+		}
+	},
+);
+
+export { router };
